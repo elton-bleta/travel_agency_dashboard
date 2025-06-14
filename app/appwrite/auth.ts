1b4d638bd17b8d5d6faf4003b3e4d4db9b4ba792
Tonii1993 <eltonbleta93@gmail.com>
2025-06-14 17:40:35 +0200

    appwrite setup

---
+++ app/appwrite/auth.ts
@@ -0,0 +1,36 @@
+export const loginWithGoogle = async () => {
+  try {
+  } catch (e) {
+    console.log(e);
+  }
+};
+export const logoutUser = async () => {
+  try {
+  } catch (e) {
+    console.log(e);
+  }
+};
+export const getUser = async () => {
+  try {
+  } catch (e) {
+    console.log(e);
+  }
+};
+export const getGooglePicture = async () => {
+  try {
+  } catch (e) {
+    console.log(e);
+  }
+};
+export const storeUserData = async () => {
+  try {
+  } catch (e) {
+    console.log(e);
+  }
+};
+export const getExistingUser = async () => {
+  try {
+  } catch (e) {
+    console.log(e);
+  }
+};
