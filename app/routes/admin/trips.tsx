386a633d6530462490b8302a057960a754d31d85
Tonii1993 <eltonbleta93@gmail.com>
2025-08-14 18:31:07 +0200

    update the tripspage

--- app/routes/admin/trips.tsx
+++ app/routes/admin/trips.tsx
@@ -1,6 +1,41 @@
-import { Header } from "components";
+import { Header, TripCard } from "components";
+import type { Route } from "./+types/trips";
+import { useSearchParams, type LoaderFunctionArgs } from "react-router";
+import { parseTripData } from "lib/utils";
+import { getAllTrips } from "~/appwrite/trips";
+import { useState } from "react";
+import { PagerComponent } from "@syncfusion/ej2-react-grids";
 
-const Trips = () => {
+export const loader = async ({ request }: LoaderFunctionArgs) => {
+  const limit = 8;
+  const url = new URL(request.url);
+  const page = parseInt(url.searchParams.get("page") || "1", 10);
+  const offset = (page - 1) * limit;
+  const { total, allTrips } = await getAllTrips(limit, offset);
+
+  return {
+    trips: allTrips.map(({ $id, tripDetail, imageUrl }) => ({
+      id: $id,
+      ...parseTripData(tripDetail),
+      imageUrls: imageUrl ?? [],
+    })),
+    total,
+  };
+};
+
+const Trips = ({ loaderData }: Route.ComponentProps) => {
+  const trips = loaderData.trips as Trip[] | [];
+
+  const [searchParams] = useSearchParams();
+  const initialPage = Number(searchParams.get("page") || "1");
+
+  const [currentPage, setCurrentPage] = useState(initialPage);
+
+  const handlePageChage = (page: number) => {
+    setCurrentPage(page);
+    window.location.search = `?page=${page}`;
+  };
+
   return (
     <main className="all-users wrapper">
       <Header
@@ -9,6 +44,39 @@ const Trips = () => {
         ctaText="Create a trip"
         ctaUrl="/trips/create"
       />
+
+      <section>
+        <h1 className="p-24-semibold text-dark-100 mb-4">
+          Manage Created Trips
+        </h1>
+        {trips.length === 0 ? (
+          <p className=" text-dark-300 mt-8">
+            No trips Found. try creatign one!
+          </p>
+        ) : (
+          <div className="trip-grid mb-4">
+            {trips.map((trip) => (
+              <TripCard
+                key={trip.id}
+                id={trip.id}
+                name={trip.name}
+                imageUrl={trip?.imageUrls[0]}
+                location={trip.itinerary?.[0]?.location ?? ""}
+                tags={[trip.interests, trip.travelStyle]}
+                price={trip.estimatedPrice}
+              />
+            ))}
+          </div>
+        )}
+
+        <PagerComponent
+          totalRecordsCount={loaderData.total}
+          pageSize={8}
+          currentPage={currentPage}
+          click={(args) => handlePageChage(args.currentPage)}
+          cssClass="!mb-4"
+        />
+      </section>
     </main>
   );
 };
--- lib/utils.ts
+++ lib/utils.ts
@@ -37,7 +37,8 @@ export function parseTripData(jsonString: string): Trip | null {
   }
 }
 
-export function getFirstWord(input: string = ""): string {
+export function getFirstWord(input?: string | null): string {
+  if (!input) return "";
   return input.trim().split(/\s+/)[0] || "";
 }
 
